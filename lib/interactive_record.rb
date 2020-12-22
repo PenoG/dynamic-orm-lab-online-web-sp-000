8a4b39d30e4a0f5203926bf620716c30c8da1c89
Mason Phillips <mjphillips2000@gmail.com>
2020-12-22 20:09:12 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+  self.table_name.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
